4db5ab530c46d985fe0643e5d74c705cfdfef14e
Aderonny <ogundipeaderonkejoy@gmail.com>
2025-04-15 15:24:20 +0100

    Update Jenkins-install.sh

--- Jenkins-install.sh
+++ Jenkins-install.sh
@@ -1,32 +1,46 @@
-# Jenkins
 
-```sh
-#!/bin/sh
+#!/bin/bash
 
-# Create a T2-MEDIUM REDHAT INSTANCE IN AWS and install Jenkins
-# The scripts/commands that follow will have Jenkins installed
+# Author: Aderonke
+# Date: 14/04/2025
+# Description: Installing Jenkins on RHEL 7/8/9, CentOS 7/8/9,ubuntu or Amazon Linux OS.
+# Note: This script can be used as user-data when launching your EC2 VM.
 
-sudo hostnamectl set-hostname auto
+# Set the timezone
+sudo timedatectl set-timezone America/New_York
 
-sudo yum -y install unzip wget tree git -y
+# Set the hostname
+sudo hostnamectl set-hostname jenkins
 
-# Add required dependencies for the Jenkins package
-sudo yum install fontconfig java-17-openjdk -y
+# Install necessary packages
+sudo yum install -y wget git nano tree vim unzip
 
+# Add Jenkins repository
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat/jenkins.repo
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+# Import Jenkins GPG key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
+# Upgrade packages
 sudo yum upgrade -y
 
-sudo yum install jenkins -y
+# Install Java (required for Jenkins)
+sudo yum install -y fontconfig java-17-openjdk
 
+# Install Jenkins
+sudo yum install -y jenkins
+
+# Reload systemd manager configuration
+sudo systemctl daemon-reexec
+
+# Enable and start Jenkins service
 sudo systemctl enable jenkins
-
 sudo systemctl start jenkins
 
+# Check Jenkins service status
 sudo systemctl status jenkins
 
+# Switch to ec2-user
 sudo su - ec2-user
-```
+
